270c456dab784fd7266a5706c4857362ce730452
Otto Laitila <otto.laitila@unboxedconsulting.com>
2015-04-15 15:18:56 +0100

    Production deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,25 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, "10.ubxd.com"
+set :scm, :copy
+set :use_sudo, false
+set :domain, ''
+set :user, 'deploy'
+set :include_dir, 'dist'
+set :tar_roles, :all
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+before :deploy, :update_code do
+  run_locally do
+    execute :rm, '-rf', 'dist/*'
+    execute :gulp 
   end
-
+end
+after :deploy, :create_release do
+    on roles(fetch(:tar_roles, :all)) do
+      # Clear up OS X resource fork spoor files
+      #execute :find, "#{release_path}/dist", '-name', '"._*"', '-print0', '|', 'xargs', '-0', 'rm'
+      #execute :rm, '-f', "#{release_path}/._dist"
+      execute :mv, "#{release_path}/dist", "#{release_path}/app"
+  end
 end
